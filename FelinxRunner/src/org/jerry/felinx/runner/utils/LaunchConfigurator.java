a37c2e89b5d2c89dbab08b06c28eb73fc55aecd5
codessentials <github@codessentials.com>
2013-01-15 13:23:43 +0100

    Updating license info.

--- FelinxRunner/src/org/jerry/felinx/runner/utils/LaunchConfigurator.java
+++ FelinxRunner/src/org/jerry/felinx/runner/utils/LaunchConfigurator.java
@@ -1,5 +1,19 @@
 /**
- * Code from http://grepcode.com/file_/repo1.maven.org/maven2/org.apache.felix/org.apache.felix.main/2.0.3/org/apache/felix/main/Main.java/?v=source
+ *  Felinx - Integration link between Felix and Eclipse
+    Copyright (C) 2013  Michiel Vermandel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jerry.felinx.runner.utils;
 
@@ -14,8 +28,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.felix.framework.util.Util;
-
+/**
+ * Code from http://grepcode.com/file_/repo1.maven.org/maven2/org.apache.felix/org.apache.felix.main/2.0.3/org/apache/felix/main/Main.java/?v=source
+ */
 public class LaunchConfigurator {
 
 	/**
