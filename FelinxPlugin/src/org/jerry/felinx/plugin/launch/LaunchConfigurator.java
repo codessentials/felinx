d18e2aeef5299549b464696a6d8f58c50a91c7f7
codessentials <github@codessentials.com>
2013-01-15 13:13:39 +0100

    Updating license info.

--- FelinxPlugin/src/org/jerry/felinx/plugin/launch/LaunchConfigurator.java
+++ FelinxPlugin/src/org/jerry/felinx/plugin/launch/LaunchConfigurator.java
@@ -1,7 +1,24 @@
 /**
- * Code from http://grepcode.com/file_/repo1.maven.org/maven2/org.apache.felix/org.apache.felix.main/2.0.3/org/apache/felix/main/Main.java/?v=source
+ *  Felinx - Integration link between Felix and Eclipse
+    Copyright (C) 2013  Michiel Vermandel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jerry.felinx.plugin.launch;
+/**
+ * Code from http://grepcode.com/file_/repo1.maven.org/maven2/org.apache.felix/org.apache.felix.main/2.0.3/org/apache/felix/main/Main.java/?v=source
+ */
 
 import java.io.File;
 import java.io.FileNotFoundException;
--- FelinxPlugin/src/org/jerry/felinx/plugin/views/ControllerView.java
+++ FelinxPlugin/src/org/jerry/felinx/plugin/views/ControllerView.java
@@ -1,15 +1,26 @@
+/**
+ *  Felinx - Integration link between Felix and Eclipse
+    Copyright (C) 2013  Michiel Vermandel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.jerry.felinx.plugin.views;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.graphics.Path;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
